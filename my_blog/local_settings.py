7c8969bab6bc450f69a991f9ad38e9da5ca88cb4
medina6 <medinakubanych@gmail.com>
2022-04-11 17:02:11 +0600

    start

---
+++ my_blog/local_settings.py
@@ -0,0 +1,14 @@
+SECRET_KEY = '8plo_@m39th1=5e%332s#w*9y9_((#$#$(u%dgry733xdx-msl'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_project',
+        'USER': 'medina',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
+}
+
+
+
